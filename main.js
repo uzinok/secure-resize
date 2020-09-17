ed87608b6f83f889d53251ad922904e590ae51a1
uzinok <uzinok@yandex.ru>
2020-09-17 12:23:43 +0400

    готово

---
+++ main.js
@@ -0,0 +1,15 @@
+window.addEventListener("resize", function () {
+    windowResize();
+  });
+
+  var check;
+
+  function windowResize() {
+    clearTimeout(check);
+    
+    check = setTimeout(function () {
+
+        // ваша функция
+
+    }, 100);
+  }
